b1096262814641b0ad21e66bc11fd25917b5a779
wesleypds <wlypereiraa@gmail.com>
2025-05-19 20:59:19 -0300

    :sparkles: feat: add entidade game

---
+++ src/main/java/com/wesleypds/dslist/entities/Game.java
@@ -0,0 +1,43 @@
+package com.wesleypds.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+
+    private String genre;
+
+    private String platforms;
+
+    private Double score;
+
+    private String imgUrl;
+
+    private String shortDescription;
+    
+    private String longDescription;
+
+}
